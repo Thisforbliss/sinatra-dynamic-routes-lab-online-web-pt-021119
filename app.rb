c963567ec4b6e809bba5f30180f860f95e571991
Malcolm Leake <echelon1089@gmail.com>
2019-06-20 00:19:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ get '/say/:number/:phrase' do
 end 
 
 get '/say/:word1/:word2/:word3/:word4/:word5' do
- parent = "HEy #{params[:word1]} #{params[:word2]} #{params[:word2]} #{params[:word2]} #{params[:word2]}."
+ parent = "HEy #{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
 binding.pry
 
 end
