80484f53bee6b20c46eec2a172a82f26115e14fa
Malcolm Leake <echelon1089@gmail.com>
2019-06-20 00:25:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
 end
 
 get '/operation/:number1/:number2' do
-   params[:number1].to_i + params[:number2].to_i
+   sum = params[:number1].to_i + params[:number2].to_i
   
 end
   
